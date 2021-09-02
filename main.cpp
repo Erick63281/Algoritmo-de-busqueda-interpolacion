0abe8385ce48008ecc4ddb2e9ed96e72472d0ceb
Erick63281 <85661165+Erick63281@users.noreply.github.com>
2021-09-01 22:28:19 -0500

    Add files via upload
    2 algoritmo el de inserción

--- main.cpp
+++ main.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 int main() {
-  //Formula de la interpolacion;
-  //medio = primero +((num - A[primero])*(ulltimo - primero))/(A[ultimo] - A[primero])
-int A[7]={1,24,3,45,67,89, 5};
-int primero = 0;
-int ultimo = 7-1;
-int medio;
-int n = 89;
-int contador = 0;
-while (A[primero] != n && contador < 7 ){
-  medio = primero + ((n - A[primero])*(ultimo - primero))/(A[ultimo] - A[primero]);
-  if(A[medio] < n){
-    primero = medio +1;
-  }
-    else if (A[medio] > n){
-    ultimo = medio - 1;
-    }
-    else {
-      primero = medio;
-      contador++;
-      break;
-    }
-    if (A[primero] == n){
-    cout<<"Elemento encontrado";
-    }
-    else {
-    cout<<"Elemento no encontrado";
-    }
-    }
+int a[50] , n = 0 , aus = 0;
+ofstream archivo;
+archivo.open("inserccion.txt", ios::out);
+cout<<"Ingrese el tamaño del arreglo: ";
+cin>>n; 
+archivo<<"Datos del arreglo: ";
+for(int i = 0 ; i < n ; i++){
+cout<<"Ingrese el arrego en la posicion "<<i+1<<": ";
+cin>>a[i];
+archivo<<a[i];
 }
+archivo<<endl;
+for(int i = 0  ; i < n ; i++){
+   aus= a[i];
+   int p = i;
+   while(p > 0){
+   if(a[p - 1] > aus){
+     a[p] = a[p - 1];
+     a[p - 1] =  aus;
+   }
+   p--;
+   }
+  }
+  archivo<<"Datos del arreglo ordenado: ";
+  for(int i = 0 ; i < n ; i++){
+  cout<<a[i]<<" ";
+  archivo<<a[i];
+  }
+}
+
